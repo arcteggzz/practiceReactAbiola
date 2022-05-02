9d3bfa4a19dbb247af39a3974041e6d35e5ed626
Oghenetega Esedere <arcteggzz@gmail.com>
2022-05-02 15:37:01 +0000

    Created and Updated NavBar and styling

--- white-uipractice01/src/components/NavBar.jsx
+++ white-uipractice01/src/components/NavBar.jsx
@@ -1,10 +1,21 @@
 function NavBar() {
     return (
-      <div className="App">
-        <h1>Nav Bar</h1>    
+      <div className={navBar}>
+          <h1 className="text-[24px] font-bold font-raleway">React Landing Page</h1>
+          <div className="flex lg:space-x-10 sm:space-x-5 md:space-x-8 text-[15px] font-sans">
+              <a href="" className={linkStyle}>Features</a>
+              <a href="" className={linkStyle}>About</a>
+              <a href="" className={linkStyle}>Services</a>
+              <a href="" className={linkStyle}>Gallery</a>
+              <a href="" className={linkStyle}>Testimonials</a>
+              <a href="" className={linkStyle}>Team</a>
+              <a href="" className={linkStyle}>Contact</a>
+          </div>
       </div>
     );
   }
-  
+
+  const linkStyle = `hover:bg-red-900`
+  const navBar = `mx-auto w-[85%] flex justify-between items-center py-5 uppercase`
 export default NavBar;
   
--- white-uipractice01/tailwind.config.js
+++ white-uipractice01/tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: "300px",
+      md: "760px",
+      lg:"1024px"
+    },
+    container: {
+      center: true,
+    },
+    fontFamily: {
+    sans : ["Lato, sans-serif"],
+    raleway: ["Raleway, sans-serif"]
+    }
   },
   plugins: [],
 }
